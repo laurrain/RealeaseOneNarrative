9c408edc6478934cdd49630e747568512571c09e
emanuel281 <emanuel@projectcodex.co>
2015-03-11 16:48:09 +0200

    Just updating popular_products_over_days() function

--- read_Nelisa_Sales_History_CSV.js
+++ read_Nelisa_Sales_History_CSV.js
@@ -29,7 +29,8 @@ function bubbleSort (array) {
 
 function write_to_file (filename, data, data_nummer) {
 	// body...
-	var file_ops = require('fs');
+
+	/*var file_ops = require('fs');
 
 	if(data_nummer === null || data_nummer === 1){ 
 		file_ops.writeFile(filename, "", function(err){
@@ -53,58 +54,16 @@ function write_to_file (filename, data, data_nummer) {
 		data.forEach(function(item){
 			file_ops.appendFile(filename, item["product"] + ';'+ item["sold_no"] + '\n', function(err){
 				if (err) throw err;
-				console.log(item["product"] + '  '+ item["sold_no"]);
+				//console.log(item["product"] + '  '+ item["sold_no"]);
 			});	
 		});
+		console.log("\n");
 	}
-
-	console.log('\n' + "Wrote to file: " + filename);
-	console.log('');
+	console.log("Wrote to file: " + filename + "\n\n");*/
 
 };
 
-function getTheInventory(filename) { //This gets the items sold the spaza shop
-	// body...
-	var fs = require('fs');
-	var buffer = fs.readFileSync(filename);
-	var list = buffer.toString().replace(/,/gi, '.');
-	var sales_history_rows = list.split('\n');
-
-	var spaza_inventory = [];
-	
-	var sales_history = sales_history_rows.map(function(row){
-		
-		var fields = row.split(";");
-
-		return {
-			day: fields[0],
-			date: fields[1],
-			stock_item: fields[2],
-			no_sold_items: fields[3],
-			sales_price: fields[4]
-		}
-
-	});
-
-
-	sales_history.forEach(function(row){
-
-		var i = 0;
-
-		spaza_inventory.forEach(function(item){
-			if(item === row['stock_item']){
-				++i;
-			}
-			
-		});
-
-		if(i === 0 && row['stock_item'] !== 'stock item'){
-			spaza_inventory.push(row['stock_item']);
-		}
-
-	});	//Getting the inventory ends here
-	write_to_file("Selling Items.csv", spaza_inventory, 1);
-
+function popular_products (spaza_inventory, sales_history) {
 	//Answering the question 'How much of each item has been sold?' starts here
 	var inventory_sold = [];
 
@@ -122,11 +81,95 @@ function getTheInventory(filename) { //This gets the items sold the spaza shop
 	});
 
 	bubbleSort(inventory_sold);
-	write_to_file("Selling Items (Sorted by Number sold).csv", inventory_sold, 2)
+	write_to_file("Selling Items (Sorted by Number sold).csv", inventory_sold, 2);
+	
+}
+
+function get_sales_history (filename) {
+	// body...
+	var fs = require('fs');
+	var buffer = fs.readFileSync(filename);
+	var list = buffer.toString().replace(/,/gi, '.');
+	var sales_history_rows = list.split('\n');
+
+	var sales_history = sales_history_rows.map(function(row){
+		
+		var fields = row.split(";");
+
+		return {
+			day: fields[0],
+			date: fields[1],
+			stock_item: fields[2],
+			no_sold_items: fields[3],
+			sales_price: fields[4]
+		}
+
+	});
+
+	return sales_history
+}
+
+function popular_products_over_days (filename, spaza_inventory, start_date, end_date) {
+	// body...
+	var sales_history = get_sales_history(filename);
+
+	var inventory_sold = [];
+	var track_date = 0;
+
+	var sold = 0, i = 1;
+	
+
+	spaza_inventory.forEach(function (item) {
+
+		sales_history.forEach(function (item_history) {
+			if(item === item_history)
+				console.log(item);
+		});
+	});
+
+	//console.log(inventory_sold);
+
+	//bubbleSort(inventory_sold);
+	//write_to_file("Testing.csv", inventory_sold, 2);
+}
+
+
+function getTheInventory(filename) { //This gets the items sold the spaza shop
+	// body...
+
+	var sales_history = get_sales_history(filename);
+
+	var spaza_inventory = [];
+
+	sales_history.forEach(function(row){
+
+		var i = 0;
+
+		spaza_inventory.forEach(function(item){
+			if(item === row['stock_item']){
+				++i;
+			}
+			
+		});
+
+		if(i === 0 && row['stock_item'] !== 'stock item'){
+			spaza_inventory.push(row['stock_item']);
+		}
+
+	});	//Getting the inventory ends here
+	//write_to_file("Selling Items.csv", spaza_inventory, 1);
+	
+	popular_products(spaza_inventory, sales_history);
+
+	popular_products_over_days(filename, spaza_inventory, "12-Feb", "16-Feb");
+
 
 };
 
+
 print_logo(function(){
+
+	var filename = 'Nelisa Sales History.csv';
 	process.stdout.write("\n" + "---------------------------------------------------------------------------------------------------" + "\n");
-	getTheInventory('Nelisa Sales History.csv');
+	getTheInventory(filename);
 });
