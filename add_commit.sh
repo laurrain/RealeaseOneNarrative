bf36ae300dafc5fed15c4705b1f0fb312c6365f8
Emanuel <emanuel@projectcodex.co>
2015-07-28 16:36:08 +0200

    modified add_commit.sh script

--- add_commit.sh
+++ add_commit.sh
@@ -5,6 +5,6 @@ git add ./;
 echo "Enter COMMIT message: ";
 read commit_message;
 git commit -m "$commit_message";
-git push origin master -f;
-ssh emanuel@ghost.projectcodex.co 'cd ReleaseOneNarrative; git pull origin master; pm2 restart app;'
-exit;
+git push origin master;
+#ssh emanuel@ghost.projectcodex.co 'cd ReleaseOneNarrative; git pull origin master; pm2 restart app;'
+#exit;
