59524004b6279baa1c244017f4037f70ba978367
Emanuel <emanuel@projectcodex.co>
2015-08-11 15:18:41 +0200

    Fixed indentation in Auth.js

--- routes/Auth.js
+++ routes/Auth.js
@@ -25,14 +25,14 @@ exports.show_supplier_popular_product = function (req, res, next) {
 
 exports.promoteUser = function(req, res, next){
 
-    var input = JSON.parse(JSON.stringify(req.body))
+    var input = JSON.parse(JSON.stringify(req.body));
         authData.promoteUser(input, function(err, results){
             if(err)
-                console.log(err)
+                console.log(err);
 
             res.redirect("/admin_panel")
-        })
-}
+        });
+};
 
 exports.addUser = function(req, res, next){
         var input = JSON.parse(JSON.stringify(req.body));
@@ -85,7 +85,7 @@ exports.addUser = function(req, res, next){
                 layout : false
             })
         }
-}
+};
 
 exports.authUser = function(req, res, next){
     past_pages = [];
@@ -93,63 +93,61 @@ exports.authUser = function(req, res, next){
       user = userData.username,
       password = userData.password;
         
-        authData.authUser(userData, function(err, results) {
-            if (err) return next(err);
+    authData.authUser(userData, function(err, results) {
+        if (err) return next(err);
 
-            if(results.length > 0){
-                bcrypt.compare(password, results[0].password,  function(err, reply){
-                    if(err)
-                        console.log("[!] There was an error with bcrypt.compare() ", err);
+        if(results.length > 0){
+            bcrypt.compare(password, results[0].password,  function(err, reply){
+                if(err)
+                    console.log("[!] There was an error with bcrypt.compare() ", err);
 
-                    if(reply && !results[0].locked){
-                        counter = 0
-                        req.session.user = results[0].username
+                if(reply && !results[0].locked){
+                    counter = 0
+                    req.session.user = results[0].username
 
-                        administrator = results[0].admin
+                    administrator = results[0].admin
 
-                        return res.redirect("/");
-                    }
-                    else{
+                    return res.redirect("/");
+                }
+                else{
 
-                        counter++;
-                        var msg = '';
-                        if(counter == 3 || results[0].locked){
-                            userData.locked = 1;
-                            authData.lock(userData, function(err, results) {
-                                if (err) return next(err);
-                            
-                                msg = "Your account has been blocked!";
-                                return res.render("login", {
-                                    message : msg,
-                                    layout : false
-                                });
-                            });
-                        }else{
-
+                    counter++;
+                    var msg = '';
+                    if(counter == 3 || results[0].locked){
+                        userData.locked = 1;
+                        authData.lock(userData, function(err, results) {
+                            if (err) return next(err);
+                        
+                            msg = "Your account has been blocked!";
                             return res.render("login", {
-                                message : msg+"Username or password incorrect!",
+                                message : msg,
                                 layout : false
                             });
-                        }
+                        });
+                    }else{
+
+                        return res.render("login", {
+                            message : msg+"Username or password incorrect!",
+                            layout : false
+                        });
                     }
-                });
-            }
-            else{
-                counter = 0
-                return res.render("login", {
-                    message : "Username doesn't exist!",
-                    layout : false
-                });
-            }
-        });
-   
-}
+                }
+            });
+        }
+        else{
+            counter = 0
+            return res.render("login", {
+                message : "Username doesn't exist!",
+                layout : false
+            });
+        }
+    });
+};
 
 exports.checkUser = function(req, res, next){
-  if (req.session.user){
+    if (req.session.user){
         return next();
-  }else{
-    res.redirect('/login');
-  
-}
+    }else{
+        res.redirect('/login');
+    }
 }
