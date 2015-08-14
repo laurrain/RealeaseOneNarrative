037ee292938b5b14fa2497deec528d850a2fe06d
Emanuel <emanuel@projectcodex.co>
2015-08-14 15:32:43 +0200

    All suppliers tests passing

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(session({secret: "yada yada", saveUninitialized : false, resave: true, cookie : {maxAge : 5*60000}}));
+app.set("x-bowered-by", false)
 
 app.get("/", auth.checkUser, function(req, res){
 
--- test/suppliers_test.js
+++ test/suppliers_test.js
@@ -15,7 +15,35 @@ describe('Testing suppliersData queries', function() {
   it('show_all_suppliers: Should return a list of suppliers', function(done) {
     suppliersData.show_all_suppliers(function(err, results){
 		// console.log(data)
-		assert.equal(results.length, 5);
+  		assert.equal(results.length, 5);
+      done();
+    });
+  });
+
+  it('show_supplier_popular_product: Should return supplier of popular product', function(done) {
+    suppliersData.show_supplier_popular_product(function(err, results){
+    // console.log(data)
+      assert.equal(results.length, 1);
+      done();
+    });
+  });
+
+  it('show_supplier_profitable_product: Should return supplier of profitable product', function(done) {
+    suppliersData.show_supplier_profitable_product(function(err, results){
+    // console.log(data)
+      assert.equal(results.length, 1);
+      done();
+    });
+  });
+
+  it('getSearchAllSuppliers: Should return supplier(s) matching the search string', function(done) {
+    suppliersData.getSearchAllSuppliers("all", function(err, results){
+    // console.log(data)
+      assert.equal(results.length, 5);
+    });
+    suppliersData.getSearchAllSuppliers("makr", function(err, results){
+    // console.log(data)
+      assert.equal(results.length, 1);
       done();
     });
   });
