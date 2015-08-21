b5b640a38926b66f8e2344645afa8d56786c4b17
Emanuel <emanuel@projectcodex.co>
2015-08-21 16:08:32 +0200

    Promise-fied all functions in file Earnings.js

--- routes/Earnings.js
+++ routes/Earnings.js
@@ -39,13 +39,17 @@ exports.show_category_earnings = function(req, res, next){
 exports.show_products_price_cost = function (req, res, next) {
     req.services(function(err, services){
         var earningData = services.earningDataServ;
-        earningData.show_products_price_cost(function(err, results){
+        earningData.show_products_price_cost()
+        .then(function(results){
             if (err) return next(err);
 
             res.render( 'products_price_cost', {
                 data : results,
                 administrator : administrator
             });
+        })
+        .catch(function(err){
+            console.log(err);
         });
     })
 };
